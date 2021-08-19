4e2f7baadecfdc0a320767abbc1b1fb34e53af7d
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-19 14:20:08 +0530

    finish hulu clone

--- hulu-clone/components/Nav.js
+++ hulu-clone/components/Nav.js
@@ -1,4 +1,4 @@
-import request from "../utill/request";
+import requests from "../utill/requests";
 import { useRouter } from 'next/router';
 
 function Nav() {
@@ -8,7 +8,7 @@ function Nav() {
     return (
         <nav className="relative">
             <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
-                {Object.entries(request).map(([key, { title, url }]) => (
+                {Object.entries(requests).map(([key, { title, url }]) => (
                     <h2 onClick={() => router.push(`/?genre=${key}`)} key={key} className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500">{title}</h2>
                 ))}
             </div>
